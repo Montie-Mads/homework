6eb8a29077bac5601eb9927a75e865226f229bfe
Montie-Mads <mmadeline551@gmail.com>
2023-02-11 09:46:25 -0500

    1-11-23

--- Lesson 08/01_compare_that/starter_code/js/index.js
+++ Lesson 08/01_compare_that/starter_code/js/index.js
@@ -1,12 +1,40 @@
 
- document.querySelector('#submit').onclick = function(e) {
-    e.preventDefault();
-     let comparison;
-     let a = Number(document.querySelector('#a').value);
-     let b = Number(document.querySelector('#b').value);
- 
-     // Add comparison logic
- 
-     document.querySelector('#comparison').innerText = comparison;
-   };
- 
+document.querySelector('#submit').onclick = function (e) {
+  e.preventDefault();
+  let comparison;
+  let a = Number(document.querySelector('#a').value);
+  let b = Number(document.querySelector('#b').value);
+
+  //u r telling javacript to turn a and b into a number
+
+  // Add comparison logic
+
+  // if a is greater than b
+  if (a > b) {
+    comparison = ">"
+  }
+
+  //if b is greater than a
+  if ( b > a ) {
+    comparison = " < ";
+  }
+
+
+  //if they are equal 2 == or 3== also work!
+  if ( b == a ) {
+    comparison = "=";
+  }
+
+  //if neither isn't a number
+  
+  if ( typeof a != "number" || typeof b  != "number") {
+    comparison = "N/A";
+  }
+
+  if (isNaN(a) || isNaN(b)) {
+    comparison ="N/A";
+  }
+
+
+  document.querySelector('#comparison').innerText = comparison;
+};
