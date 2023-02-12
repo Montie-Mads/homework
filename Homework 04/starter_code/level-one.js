95467b3f354dee96b967be223ca785b5d0ed9122
Montie-Mads <mmadeline551@gmail.com>
2023-02-12 16:13:32 -0500

    hw4-2-12-23

--- Homework 04/starter_code/level-one.js
+++ Homework 04/starter_code/level-one.js
@@ -14,10 +14,11 @@ ballEl.addEventListener('click', oneUp)
 function oneUp() {
   //when ball is clicked, add 10
   score += 10;
-  //when the ball is clicked, add 10 and add to score
+  //when the ball is clicked, add 10 and add to the score. the  innerText is targeting the 0000 h2 class
   scoreEl.innerText = score;
   //if the score is greater than or equal to 100, the user wins the game!
   if (score >= 100) {
+    //when the user reaches 100 the user will be declared the winner
     declareWinner();
   }
 }
