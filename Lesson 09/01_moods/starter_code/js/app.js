358498495d983a31cde651a04ebe169fd8abd803
Montie-Mads <mmadeline551@gmail.com>
2023-02-16 17:34:26 -0500

    2-15-23

--- Lesson 09/01_moods/starter_code/js/app.js
+++ Lesson 09/01_moods/starter_code/js/app.js
@@ -1,19 +1,38 @@
-//wait for the DOM elements to load before executing
+//wait for the DOM elements to load before executing this makes it so the site loads before running any javaScript
 
-    // Create a function that runs whenever the submit button is clicked
+document.addEventListener('DOMContentLoaded', function () {
 
-        //prevent the submit button from refreshing the page
+});
 
+// Create a function that runs whenever the submit button is clicked
+document.querySelector("#submit-btn").addEventListener('click', function (e) {
+    console.log(e);
+    //prevent the submit button from refreshing the page
+    e.preventDefault();
 
-        //Create a variable called moodvalue and get the value of the #mood input
+    //Create a variable called moodvalue and get the value of the #mood input
+    let moodvalue = document.querySelector('#mood').value;
+    console.log(moodvalue)
 
+    //Correct for capitalization
+    moodvalue = moodvalue.trim().toLowerCase();
 
-        //Correct for capitalization
+    // if the user inputs excited / ecstatic / fantastic change the CSS class to 'excited'
+    if (moodvalue == 'excited' || moodvalue == 'estatic' || moodvalue == 'fantastic') {
+        document.querySelector('.moodring > div').setAttribute('class', 'excited')
+    }
 
-        // if the user inputs excited / ecstatic / fantastic change the CSS class to 'excited'
+    // if the user inputs happy/good/great change the CSS class to 'happy'
+    if (moodvalue == 'happy' || moodvalue == 'good' || moodvalue == 'great') {
+        document.querySelector('.moodring > div').setAttribute('class', 'happy')
+    }
 
-        // if the user inputs happy/good/great change the CSS class to 'happy'
-
-        // if the user inputs bad/angry change the CSS class to 'bad'
+    // if the user inputs bad/angry change the CSS class to 'bad'
+    if (moodvalue == 'bad' || moodvalue == 'angry') {
+        document.querySelector('.moodring > div').setAttribute('class', 'bad')
+    }
 
     // Listen for user interaction on the submit button.
+
+
+})
