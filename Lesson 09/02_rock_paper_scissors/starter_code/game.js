f203426bea68ced34f1a27456d466327ce2ae220
Montie-Mads <mmadeline551@gmail.com>
2023-02-16 17:34:26 -0500

    2-15-23

--- Lesson 09/02_rock_paper_scissors/starter_code/game.js
+++ Lesson 09/02_rock_paper_scissors/starter_code/game.js
@@ -2,8 +2,122 @@
 
 
 
+// // Create a function that determines the computer choice.  It should return one of the three values: 'rock', 'paper', or 'scissors'
+
+
+function computerChoice() {
+
+    //make a random number between 1 and 3
+    let = weNeedAName = Math.ceil(Math.random() * 3);
+    // if number 1, return rock
+    if (weNeedAName == 1) {
+        return "rock";
+    } else if (weNeedAName == 2) {
+        // if number is 2 return scissors
+        return "scissors";
+    } else {
+        return "paper";
+    }
+
+
+    //else return paper
+};
+
+
+/* Create a function that compares the user's selection to the computer's selection.  It should then output the opponent's choice to #computerSelection and display who is the winner.
+
+ */
+function compare(me, opponent) {
+
+    let winner = "";
+
+    //Decide who wins
+
+    // if me == oponent 
+    if (me == opponent) {
+        winner = "Tie!";
+    } else if (me == paper) {
+        //if me is paper opponent picks rock, i win
+        if (opponent == "rock") {
+            winner = "I win!"
+        } else {
+            winner == "Computer Wins!"
+        }
+    } else if (me == "rock") {
+        //If me is paper and opp is rock, I win
+        if (opponent == "paper") {
+            winner = "Computer Wins"
+        } else {
+            winner = "I win!"
+        }
+    } else {
+        //Assume I picked scissors
+        if (opponent == "rock") {
+            winner = "Computer Wins"
+        } else {
+            winner = "I win!"
+        }
+    }
+
+
+
+    //display winner
+    document.getElementById('decision').innerText = winner;
+    // display what the computer picked  
+    document.getElementById('computerSelection').innerText = opponent;
+
+};
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+
+
+
+    // Bind a click event onto each of the three buttons.  It should run the compare function and then put the result to the #decsision id in the html
+
+    document.querySelector('#scissors').addEventListener('click', function (e) {
+        e.preventDefault();
+        compare("scissors", computerChoice())
+    });
+
+    document.querySelector('#paper').addEventListener
+        ('click', function (e) {
+            e.preventDefault();
+            compare("paper", computerChoice())
+        });
+
+    document.querySelector('#rock').addEventListener
+        ('click', function (e) {
+            e.preventDefault();
+            compare("rock", computerChoice())
+        });
+
+
+});
+
+
+// //Tor's Code to compare/take notes
+
+
+
 // Create a function that determines the computer choice.  It should return one of the three values: 'rock', 'paper', or 'scissors'
 function computerChoice() {
+
+    // Make random number between 1 and 3
+
+    let weNeedAName = Math.ceil(Math.random() * 3);
+
+    // if number is 1, return rock
+    if (weNeedAName == 1) {
+        return "rock";
+    } else if (weNeedAName == 2) {
+        // if number is 2, return scissors
+        return "scissors";
+    } else {
+        // else return paper
+        return "paper";
+    }
 
 };
 
@@ -13,13 +127,64 @@ function computerChoice() {
  */
 function compare(me, opponent) {
 
+    let winner = "";
+
+    // Decide Who Wins
+
+    // if me == opponent
+    if (me == opponent) {
+        winner = "Tie!";
+    } else if (me == "paper") {
+        // If me is paper and opp is rock, I win
+        if (opponent == "rock") {
+            winner = "I win!"
+        } else {
+            winner = "Computer Wins!"
+        }
+    } else if (me == "rock") {
+        // If me is paper and opp is rock, I win
+        if (opponent == "paper") {
+            winner = "Computer Wins!"
+        } else {
+            winner = "I win!"
+        }
+    } else {
+        // Assume I picked scissors
+        if (opponent == "rock") {
+            winner = "Computer Wins!"
+        } else {
+            winner = "I win!"
+        }
+    }
+
+
+    // Display the winner
+    document.getElementById('decision').innerText = winner;
+    // Display what the computer picked
+    document.getElementById('computerSelection').innerText = opponent;
 };
 
 
 
-jQuery( document ).ready(function() {
+document.addEventListener('DOMContentLoaded', function () {
 
-    // Bind a click event onto each of the three buttons.  It should run the compare function and then put the result to the #decsision id in the html
+    // Bind a click event onto each of the three buttons. 
+
+    document.querySelector('#scissors').addEventListener('click', function (e) {
+        e.preventDefault();
+        compare("scissors", computerChoice())
+    });
+
+    document.querySelector('#paper').addEventListener('click', function (e) {
+        e.preventDefault();
+        compare("paper", computerChoice())
+    });
+
+    document.querySelector('#rock').addEventListener('click', function (e) {
+        e.preventDefault();
+        compare("rock", computerChoice())
+    });
+
 
 
 });
