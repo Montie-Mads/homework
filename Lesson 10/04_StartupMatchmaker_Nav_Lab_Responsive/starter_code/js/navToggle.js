96c169ba4d786db5add6b56942d98db7f91ed62e
Montie-Mads <mmadeline551@gmail.com>
2023-02-20 10:45:47 -0500

    Lesson10-review

--- Lesson 10/04_StartupMatchmaker_Nav_Lab_Responsive/starter_code/js/navToggle.js
+++ Lesson 10/04_StartupMatchmaker_Nav_Lab_Responsive/starter_code/js/navToggle.js
@@ -1,4 +1,13 @@
-document.addEventListener('DOMContentLoaded', function(event) {
+document.addEventListener('DOMContentLoaded', function (event) {
+
+    //target the nav-toggle and add an event listener function to it. don't forget to add the # for the id nav-toggle because i always forget
+    document.querySelector("#nav-toggle").addEventListener('click', function (e) {
+
+        // make it so that when it is clicked, it does not refresh the page with (e).preventDefault(); 
+        e.preventDefault();
+        //toggle the nav to show/hide when the hamburger id clicked
+        document.querySelector(".main-nav").classList.toggle("main-nav-mobilehide")
+    });
 
 
 });
