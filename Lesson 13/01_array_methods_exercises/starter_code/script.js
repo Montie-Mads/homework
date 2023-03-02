bd47ed9eb0dea0836ef9621d1edd398acd69ecbf
Montie-Mads <mmadeline551@gmail.com>
2023-03-02 17:29:36 -0500

    2-1-23

--- Lesson 13/01_array_methods_exercises/starter_code/script.js
+++ Lesson 13/01_array_methods_exercises/starter_code/script.js
@@ -6,20 +6,22 @@ const fruits = ["banana", "orange", "apple"];
 // 1
 // You can ask for the index number of any array item
 // Print the index of "orange" to the console
-
+console.log(fruits[1]);
 
 // 2
 // Print the length of the array to the console
-
+console.log(fruits.length);
 
 // 3
 // .pop() removes the last item
 // Print the removed item and the new array to the console
-
-
+console.log(fruits.pop());
+console.log(fruits);
 // 4
 // .shift() removes the first item
 // Print both the removed item and the new array to the console
+console.log(fruits.shift());
+console.log(fruits);
 
 
 // 5
@@ -27,12 +29,17 @@ const fruits = ["banana", "orange", "apple"];
 // Add "kiwi" to the end of the array
 // Print the new array to the console
 
+fruits.push("kiwi")
+console.log(fruits);
+
 
 // 6
 // .unshift() adds whatever you pass into the method parenthesis to the beginning of an array
 // Add "cherry" to the front of the array
 // Print the new array to the console
 
+fruits.unshift("cherry");
+console.log(fruits);
 
 // 7
 // .splice() to adjust array values (REMOVE)
@@ -42,6 +49,8 @@ const fruits = ["banana", "orange", "apple"];
 // Remove the first item from the array
 // Print the new array to the console
 
+fruits.splice(0, 1);
+console.log(fruits);
 
 // 8
 // .splice() to adjust array values (ADD)
@@ -52,15 +61,23 @@ const fruits = ["banana", "orange", "apple"];
 // Add "pear" as the second item in the array
 // Print the new array to the console
 
+fruits.splice(1, 0, "pear");
+console.log(fruits)
 
 // 9
 // reverse() reverses the order of the items in the array without modifying them
 // Print your reversed array to the console
 
+fruits.reverse();
+console.log(fruits)
+
 
 // 10
 // join()  combines - or concatenates - an array into a string
 // The string is separated by whatever you pass into the parentheses
+
+console.log(fruits.join(" and "));
+
 
 // Join all the fruit together by the word "and" - dont forget spaces
 // Print your new string to the console
@@ -76,8 +93,10 @@ const produce = [["kiwi", "pear", "cherry"], ["broccoli", "celery", "carrots"]];
 // Second square bracker - item
 // Print the entire first nested array to the console
 
+console.log(produce);
 
 // 12
 // Using bracket notation, print the second item of the first array to the console
 // Using bracket notation, print the first item of the second array to the console
 
+console.log(produce[0][1]);
