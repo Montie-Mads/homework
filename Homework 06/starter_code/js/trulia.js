b138caca774fb6169f3a6b511949df27fbcc9bcd
Montie-Mads <mmadeline551@gmail.com>
2023-03-05 21:29:10 -0500

    hw-6-attempt

--- Homework 06/starter_code/js/trulia.js
+++ Homework 06/starter_code/js/trulia.js
@@ -4,27 +4,55 @@ let truliaCards = document.getElementsByClassName("trulia-grid-item");
 
 
 
-document.addEventListener('DOMContentLoaded', function(event) {
-
-// Toggle the navigation
 
 
+document.addEventListener('DOMContentLoaded', function (event) {
+  //select the trulia-nav-toggle and add an event listner to it
+  document.querySelector(".trulia-nav-toggle").addEventListener('click', function (e) {
+    //prevent the click from refreshing the page
+    e.preventDefault();
+    // Toggle the navigation
+    let toggleNav = document.querySelector('.trulia-nav > nav ul');
+    //loop the function to hide the nav
+    for (let i = 0; i < toggleNav.length; i++) {
+      toggleNav[i].classList.toggle('trulia-nav-mobilehide');
+    }
+
+
+  });
+
+
+  // function toggleNav() {
+  //   const navs = document.querySelector('.trulia-nav-toggle')
+  //   navs.forEach(nav.classList.toggle('.trulia-nav-mobilehide'));
+  //   document.querySelector('trulia-nav-toggle').addEventListener('click', toggleNav)
+
+  // }
 
 
 
 
-// Loop through all the cards
-    
-  // Add a click listener on each card
-      
+
+  for (let j = 0; j < truliaCards.length; j++) {
+
+    // Add a click listener on each card
+    truliaCards[j].addEventListener('click', function () {
+
       // Remove the featured class
-
+      for (let j = 0; j < truliaCards.length; j++) {
+        truliaCards[j].classList.remove('trulia-featured-grid-item');
+      }
 
       // Add the featured class on the one clicked on
+      this.classList.add('trulia-featured-grid-item');
+    });
+  }
 
 
 
-  });  
+
+
+});
 
 
 
