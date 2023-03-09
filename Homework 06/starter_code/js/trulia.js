49b8201d4920252dc0d11d0114085a067cb4612b
Montie-Mads <mmadeline551@gmail.com>
2023-03-08 20:43:40 -0500

    3-8-23

--- Homework 06/starter_code/js/trulia.js
+++ Homework 06/starter_code/js/trulia.js
@@ -1,21 +1,17 @@
 
-
 let truliaCards = document.getElementsByClassName("trulia-grid-item");
-
-
-
-
 
 document.addEventListener('DOMContentLoaded', function (event) {
   //select the trulia-nav-toggle and add an event listner to it
   document.querySelector(".trulia-nav-toggle").addEventListener('click', function (e) {
     //prevent the click from refreshing the page
     e.preventDefault();
-    // Toggle the navigation
+
     let toggleNav = document.querySelector('.trulia-nav > nav ul');
-    
+
     for (let i = 0; i < toggleNav.length; i++) {
       toggleNav[i].classList.toggle('trulia-nav-mobilehide');
+      console.log(toggleNav)
     }
 
 
